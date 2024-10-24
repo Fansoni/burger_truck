8bdf9f216815f1bf13d361f88fe215a1de318263
Fansoni Muzanzo <fansonidealmeida@gmail.com>
2024-10-24 22:56:07 +0100

    chod: reset changes

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
-## Bugger Truck - Fansoni Muzanzo
 
-#### Tecnologias Utilizadas
-Para o desenvolvimento dessa aplicação móvel, especificamente desenvolvida para a plataforma Android, foram utilizadas as seguintes tecnologias:
+# Burger Truck
 
-- Flutter v1
-- Adobe XD para o design
+Burger Truck is the second of 12 UI Flutter designs. So, let's enjoy!
 
-Desenvolvido por Fansoni Muzanzo.
+## Getting Started
+
+First, clone the repository on your local machine.
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with this application:
+
+ 1. `flutter clone https://github.com/Fansoni/burger_truck.git`
+ 2. `flutter pub get`
+
+Here are some of the technologies used for this project:
+
+ - Flutter 3.10.6
+ - Dart 3.0.6
+
+## Preview
+
+| Detail 1 | Detail 2 |
+|--|--|
+| ![enter image description here](https://github.com/Fansoni/burger_truck/blob/master/assets/images/detail1.png?raw=true) | ![enter image description here](https://github.com/Fansoni/burger_truck/blob/master/assets/images/detail2.png?raw=true) |
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:burger_truck/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyApp(),
@@ -12,42 +13,36 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  AnimationController? _controller;
-  Animation? carouselAnimation;
+class _MyAppState extends State<MyApp>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation carouselAnimation;
 
   int index = 0;
 
   var photoCarousel = [
-    'assets/images/1.jpg',
-    'assets/images/2.jpg',
-    'assets/images/3.jpg',
-    'assets/images/4.jpg',
-    'assets/images/5.jpg',
-    'assets/images/6.jpg',
+    'assets/images/1.jpg', 'assets/images/2.jpg', 'assets/images/3.jpg',
+    'assets/images/4.jpg', 'assets/images/5.jpg', 'assets/images/6.jpg',
   ];
 
   @override
   void initState() {
     super.initState();
-    _controller =
-        AnimationController(duration: Duration(seconds: 18), vsync: this);
+    _controller = AnimationController(duration: Duration(seconds: 18), vsync: this);
 
-    carouselAnimation =
-        IntTween(begin: 0, end: photoCarousel.length - 1).animate(_controller!)
-          ..addListener(() {
-            setState(() {
-              index = carouselAnimation!.value;
-            });
-          });
+    carouselAnimation = IntTween(begin: 0, end: photoCarousel.length - 1).animate(_controller)..addListener(() {
+      setState(() {
+        index = carouselAnimation.value;
+      });
+    });
 
-    _controller!.repeat();
+    _controller.repeat();
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller!.dispose();
+    _controller.dispose();
   }
 
   @override
@@ -65,8 +60,9 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                     height: 210.0,
                     decoration: BoxDecoration(
                       image: DecorationImage(
-                          image: AssetImage(photoCarousel[index]),
-                          fit: BoxFit.cover),
+                        image: AssetImage(photoCarousel[index]),
+                        fit: BoxFit.cover
+                      ),
                     ),
                   ),
                   Container(
@@ -77,16 +73,8 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        IconButton(
-                            icon: Icon(
-                              Icons.arrow_back_ios,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {}),
-                        IconButton(
-                            icon:
-                                Icon(Icons.favorite, color: Colors.pinkAccent),
-                            onPressed: () {}),
+                        IconButton(icon: Icon(Icons.arrow_back_ios, color: Colors.white,), onPressed: (){}),
+                        IconButton(icon: Icon(Icons.favorite, color: Colors.pinkAccent), onPressed: (){}),
                       ],
                     ),
                   ),
@@ -95,48 +83,15 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                     left: 20.0,
                     child: Row(
                       children: <Widget>[
-                        Icon(
-                          Icons.star,
-                          size: 20.0,
-                          color: Colors.orange[700],
-                        ),
-                        Icon(
-                          Icons.star,
-                          size: 20.0,
-                          color: Colors.orange[700],
-                        ),
-                        Icon(
-                          Icons.star,
-                          size: 20.0,
-                          color: Colors.orange[700],
-                        ),
-                        Icon(
-                          Icons.star,
-                          size: 20.0,
-                          color: Colors.orange[700],
-                        ),
-                        Icon(
-                          Icons.star,
-                          size: 20.0,
-                          color: Colors.grey,
-                        ),
-                        SizedBox(
-                          width: 8.0,
-                        ),
-                        Text(
-                          '4.7',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          width: 17.0,
-                        ),
-                        SelectedPhoto(
-                          photoLength: photoCarousel.length,
-                          indexSelected: index,
-                        ),
+                        Icon(Icons.star, size: 20.0, color: Colors.orange[700],),
+                        Icon(Icons.star, size: 20.0, color: Colors.orange[700],),
+                        Icon(Icons.star, size: 20.0, color: Colors.orange[700],),
+                        Icon(Icons.star, size: 20.0, color: Colors.orange[700],),
+                        Icon(Icons.star, size: 20.0, color: Colors.grey,),
+                        SizedBox(width: 8.0,),
+                        Text('4.7', style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),),
+                        SizedBox(width: 17.0,),
+                        SelectedPhoto(photoLength: photoCarousel.length, indexSelected: index,),
                       ],
                     ),
                   ),
@@ -150,77 +105,65 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                     Text(
                       'OPEN NOW UNTIL 7PM',
                       style: TextStyle(
-                          color: Colors.grey,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.0),
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0
+                      ),
                     ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
+                    SizedBox(height: 10.0,),
                     Text(
                       'The Cinnamon Snail',
                       style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 30.0),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0
+                      ),
                     ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
+                    SizedBox(height: 10.0,),
                     Row(
                       children: <Widget>[
                         Text(
                           '17th st & Union Sq East',
-                          style: TextStyle(color: Colors.grey, fontSize: 16.0),
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 16.0
+                          ),
                         ),
-                        SizedBox(
-                          width: 8.0,
-                        ),
-                        Icon(
-                          Icons.location_on,
-                          size: 19,
-                        ),
-                        SizedBox(
-                          width: 4.0,
-                        ),
+                        SizedBox(width: 8.0,),
+                        Icon(Icons.location_on, size: 19,),
+                        SizedBox(width: 4.0,),
                         Text(
                           '400ft Away',
                           style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16.0),
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0
+                          ),
                         ),
                       ],
                     ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
+                    SizedBox(height: 10.0,),
                     Row(
                       children: <Widget>[
-                        Icon(
-                          Icons.wifi,
-                          size: 19,
-                          color: Colors.green[600],
-                        ),
-                        SizedBox(
-                          width: 4.0,
-                        ),
+                        Icon(Icons.wifi, size: 19, color: Colors.green[600],),
+                        SizedBox(width: 4.0,),
                         Text(
                           'Location confirmed by 3 users today',
                           style: TextStyle(
-                              color: Colors.green[600],
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14.0),
+                            color: Colors.green[600],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0
+                          ),
                         ),
                       ],
                     ),
-                    SizedBox(
-                      height: 60.0,
-                    ),
+                    SizedBox(height: 60.0,),
                     Text(
                       'FEATURED ITEMS',
                       style: TextStyle(
-                          color: Colors.grey,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.0),
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0
+                      ),
                     ),
                   ],
                 ),
@@ -228,58 +171,42 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
               FeaturedItem(
                 image: 'assets/images/1.jpg',
                 name: 'Maple Mustard Tempeh',
-                description:
-                    'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
+                description: 'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
                 price: 11.25,
               ),
-              SizedBox(
-                height: 10.0,
-              ),
+              SizedBox(height: 10.0,),
               FeaturedItem(
                 image: 'assets/images/2.jpg',
                 name: 'Maple Mustard Tempeh',
-                description:
-                    'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
+                description: 'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
                 price: 12.25,
               ),
-              SizedBox(
-                height: 10.0,
-              ),
+              SizedBox(height: 10.0,),
               FeaturedItem(
                 image: 'assets/images/3.jpg',
                 name: 'Maple Mustard Tempeh',
-                description:
-                    'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
+                description: 'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
                 price: 13.25,
               ),
-              SizedBox(
-                height: 10.0,
-              ),
+              SizedBox(height: 10.0,),
               FeaturedItem(
                 image: 'assets/images/4.jpg',
                 name: 'Maple Mustard Tempeh',
-                description:
-                    'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
+                description: 'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
                 price: 14.25,
               ),
-              SizedBox(
-                height: 10.0,
-              ),
+              SizedBox(height: 10.0,),
               FeaturedItem(
                 image: 'assets/images/5.jpg',
                 name: 'Maple Mustard Tempeh',
-                description:
-                    'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
+                description: 'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
                 price: 15.25,
               ),
-              SizedBox(
-                height: 10.0,
-              ),
+              SizedBox(height: 10.0,),
               FeaturedItem(
                 image: 'assets/images/6.jpg',
                 name: 'Maple Mustard Tempeh',
-                description:
-                    'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
+                description: 'Madsahdgasjhd hjagsdhhasdghasgd hjagdjhasgjhdgash gjhagsdjhg hdgsad hdghsd',
                 price: 16.25,
               ),
             ],
@@ -293,11 +220,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
 class FeaturedItem extends StatelessWidget {
   final name, description, price, image;
   const FeaturedItem({
-    Key? key,
-    this.name,
-    this.description,
-    this.price,
-    this.image,
+    Key key, this.name, this.description, this.price, this.image,
   }) : super(key: key);
 
   @override
@@ -310,14 +233,14 @@ class FeaturedItem extends StatelessWidget {
             width: 100.0,
             height: 110.0,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(10.0),
-                  bottomRight: Radius.circular(10.0)),
-              image:
-                  DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+              borderRadius: BorderRadius.only(topRight: Radius.circular(10.0), bottomRight: Radius.circular(10.0)),
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover
+              ),
             ),
           ),
-          SizedBox(width: 20.0),
+          SizedBox(width: 20.0,),
           Expanded(
             child: Container(
               padding: EdgeInsets.only(right: 10.0),
@@ -326,25 +249,27 @@ class FeaturedItem extends StatelessWidget {
                 children: <Widget>[
                   Text(
                     name,
-                    style:
-                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                    ),
                   ),
-                  SizedBox(
-                    height: 8.0,
-                  ),
+                  SizedBox(height: 8.0,),
                   Text(
                     description,
-                    style: TextStyle(fontSize: 12.0, color: Colors.grey),
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.grey
+                    ),
                   ),
-                  SizedBox(
-                    height: 8.0,
-                  ),
+                  SizedBox(height: 8.0,),
                   Text(
                     '\$ ' + price.toString(),
                     style: TextStyle(
-                        fontSize: 18.0,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.bold),
+                      fontSize: 18.0,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold
+                    ),
                   ),
                 ],
               ),
@@ -359,22 +284,21 @@ class FeaturedItem extends StatelessWidget {
 class SelectedPhoto extends StatelessWidget {
   final photoLength, indexSelected;
 
-  const SelectedPhoto({Key? key, this.photoLength, this.indexSelected})
-      : super(key: key);
+  const SelectedPhoto({Key key, this.photoLength, this.indexSelected}) : super(key: key);
 
-  Widget _dot(isSelected) {
+  Widget _dot(isSelected){
     return Container(
       margin: EdgeInsets.only(left: 3.0, right: 3.0),
       height: 10.0,
       width: 10.0,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(5.0)),
-        color: (isSelected) ? Colors.white : Colors.grey,
+        color: (isSelected)?Colors.white:Colors.grey,
       ),
     );
   }
-
-  List<Widget> _buildDots() {
+  
+  List<Widget> _buildDots(){
     List<Widget> list = [];
     for (var i = 0; i < photoLength; i++) {
       list.add(_dot((i == indexSelected)));
@@ -385,6 +309,8 @@ class SelectedPhoto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-        mainAxisAlignment: MainAxisAlignment.center, children: _buildDots());
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: _buildDots()
+    );
   }
 }
